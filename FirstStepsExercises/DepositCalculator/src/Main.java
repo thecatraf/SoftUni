ae95b310d4ad8fe6e95bf70d8ebca979593f709c
Iliana Bakalova <thecatraf@gmail.com>
2022-05-22 17:23:44 +0300

    First Steps - more exercises

---
+++ FirstStepsExercises/DepositCalculator/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+   	Scanner scanner = new Scanner(System.in);
+   	double deposit = Double.parseDouble(scanner.nextLine());
+   	int months = Integer.parseInt(scanner.nextLine());
+   	double lihva = Double.parseDouble(scanner.nextLine())/100;
+   	double sum = deposit + months* ((deposit*lihva)/12);
+   	
+   	
+   	System.out.println(sum);
+
+       }
+
+}
---
+++ FirstStepsExercises/SuppliesForSchool/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+   	int pens = Integer.parseInt(scanner.nextLine());
+   	int markers = Integer.parseInt(scanner.nextLine());
+   	int litres = Integer.parseInt(scanner.nextLine());
+   	double discount = Double.parseDouble(scanner.nextLine())/100;
+
+   	double sum = pens * 5.8 + markers * 7.2 + litres * 1.2;
+   	double finalSum = sum - (sum*discount);
+   	System.out.println(finalSum);
+    	}
+    }
+
+
